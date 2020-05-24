050db2acb4bb9dd6bb4f6080d913a997d760eb98
prateek1556 <ppanjwani0751@gmail.com>
2020-05-24 17:21:05 +0530

    Adding Sentimental analysis of comment in API

--- src/app.py
+++ src/app.py
@@ -1,32 +1,27 @@
 from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
-from process_text import clean_text
-
-
-app = Flask(__name__)
-api = Api(app)
-
-items = []
-
-from flask import Flask, request
 from flask_restful import Resource, Api
-from process_text import *
+from process_text import clean_text
+from text_classification import find_sentiment
 
 
 app = Flask(__name__)
 api = Api(app)
 
+app = Flask(__name__)
+api = Api(app)
 
-class CleanText(Resource):
+
+class TextAnalysis(Resource):
     
     def post(self):
         data = request.get_json(silent=True)
         clean_data  = clean_text(data['text'])
-        return {'text': clean_data}
-    
+        senti = find_sentiment(clean_data)
+        getsentiment = 'Positive' if senti else 'Negative'
+        return {'text': clean_data,'sentiment':getsentiment}
+       
 
-api.add_resource(CleanText,'/cleandata')
-
+api.add_resource(TextAnalysis,'/cleandata')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True) # it is although default
