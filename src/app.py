fd0d1eda5bb018b28d8a6a17bf1bd2fa5ca287f1
prateek1556 <ppanjwani0751@gmail.com>
2020-05-26 18:57:14 +0530

    Adding Linear Regression Housing Prices prediction(post req) and model details(get req) API

--- src/app.py
+++ src/app.py
@@ -1,15 +1,15 @@
+#Flask Packages
 from flask import Flask, request
 from flask_restful import Resource, Api
+
+#Developer defined
 from process_text import clean_text
 from text_classification import find_sentiment
+from linear_regression_housingprice import predict_housing_price, housing_price_model_details
 
 
 app = Flask(__name__)
 api = Api(app)
-
-app = Flask(__name__)
-api = Api(app)
-
 
 class TextAnalysis(Resource):
     
@@ -18,10 +18,27 @@ class TextAnalysis(Resource):
         clean_data  = clean_text(data['text'])
         senti = find_sentiment(clean_data)
         getsentiment = 'Positive' if senti else 'Negative'
-        return {'text': clean_data,'sentiment':getsentiment}
-       
+        return {'text': clean_data,'sentiment':getsentiment}, 200 if clean_data else 404
+
+
+class LR_HousingPrice(Resource):
+    def post(self):
+         data = request.get_json() #x = np.array(data).tolist() # a = np.array([1,2,3,4,5]).tolist()
+         a=data['a']
+         b=data['b']
+         c=data['c']
+         d=data['d']
+         e=data['e']
+         house_price_pred = predict_housing_price(a,b,c,d,e).tolist()
+         return {'price': house_price_pred}, 200 if house_price_pred else 404
+         
+    def get(self):
+        MEA, MSE, RMSE, MAPE, coeff_df = housing_price_model_details() #, MSE, RMSE, MAPE = housing_price_model_details()
+        return {'MEA': MEA, 'MSE':MSE, 'RMSE':RMSE, 'MAPE':MAPE,'coeff_df':coeff_df.to_json()}
+        
 
 api.add_resource(TextAnalysis,'/cleandata')
+api.add_resource(LR_HousingPrice,'/lrmodel')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True) # it is although default
